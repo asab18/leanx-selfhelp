fdccb10dd8c082d549b41a59f8eb234d5f0cb0c0
Alya Sabrina <alyasabrina@Alyas-MacBook-Air.local>
2025-08-06 17:47:27 +0800

    update: intro, components

--- knowledge-site/astro.config.mjs
+++ knowledge-site/astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
+		react(),
 		starlight({
 			title: 'Leanx Ultimate Guide',
 			sidebar: [
---
+++ knowledge-site/src/components/ExternalButton.jsx
@@ -0,0 +1,14 @@
+// import { LinkButton } from "@astrojs/starlight/components";
+
+// export const ExternalButton = ({ href, children }) => (
+//     <LinkButton
+//     href={href}
+//     target="_blank"
+//     icon="external"
+//     iconPlacement="start"
+//     variant="secondary"
+//     >
+//         {children}
+//     </LinkButton>
+// );
+
