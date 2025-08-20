434ad70b191a0ec82e855a07d4975c9dddbe3718
Alya Sabrina <alyasabrina@Alyas-MacBook-Air.local>
2025-08-20 15:51:31 +0800

    update: content, constants

--- knowledge-site/src/constants.ts
+++ knowledge-site/src/constants.ts
@@ -1,6 +1,34 @@
 // TODO see how to convert to env file so that other projects can use it too 
+// TODO complete fields
+// FOR COPYING export const
 
-// TODO branding and names etc
+// Branding
+export const BRAND_NAME = 'Leanx';
+export const PRODUCT_NAME_PL = 'Payment Link';
+export const PRODUCT_NAME_CP = 'Cloud Payment';
+export const PRODUCT_NAME_PO = 'Payout'; 
+
+// Links
+export const LINK_PORTAL = '';
+
+export const LINK_PORTAL_LOGIN = '';
+export const LINK_PORTAL_REGISTER = '';
+export const LINK_PORTAL_FORGOTPW = '';
+
+export const LINK_PORTAL_COLL_LIST = '';
+export const LINK_PORTAL_PAYMENT_LIST = '';
+
+export const LINK_PORTAL_PROD_MANAGE = '';
+export const LINK_PORTAL_PROD_CAT = '';
+export const LINK_PORTAL_PROD_SUBCAT = '';
+
+export const LINK_PORTAL_SF_SET = '';
+export const LINK_PORTAL_SF_PROD = '';
+
+
+// Support
 export const SUPPORT_EMAIL =  'support@leanx.io';
+export const SUPPORT_PHONE = '';
+
 
 
