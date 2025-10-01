92e0ac5451d49c0f8ffc37a49ae4159f9e2c1afd
alya-sabrina <alya.sabrina@leanx.io>
2025-10-01 18:03:57 +0800

    update: support, dashboard, bank

--- knowledge-site/src/constants.ts
+++ knowledge-site/src/constants.ts
@@ -33,10 +33,15 @@ export const LINKS = {
 export const MESSAGES = {
     emailSales: "subject=Lean.x%20Sales%20Inquiry&body=Hello%20Lean.x%20Team,%0A%0A",
     wsSales: "text=Hello%20Lean.x%20Team,%0A%0A*My%20Details*%0AName:%0ACompany%20Name:%0AEmail:%0ATopic:%0A",
+
     emailSupport: "subject=Lean.x%20Support%20Inquiry&body=Hello%20Lean.x%20Team,",
     wsSupport: "text=Hello%20Lean.x%20Team,%20I%20need%20assistance.%0A%0A",
+
     emailIntlSupport: "subject=Lean.x%20International%20Support%20Inquiry&body=Hello%20Lean.x%20Team,",
     wsIntlSupport: "text=Hello%20Lean.x%20Team,%20I%20am%20an%20international%20user%20and%20need%20assistance.%0A%0A",
+
+    emailCgBank: "subject=Request%20to%20Change%20Linked%20Bank%20Account&body=Hello%20Lean.x%20Team,%0A%0ABank:%0AAccount%20Name:%0AAccount%20Number:%0AAttach%20latest%203%20months%20bank%20statement.",
+    wsCgBank: "text=Hello%20Lean.x%20Team,%20I%20want%20to%20change%20my%20linked%20bank%20account.%0A%0ABank:%0AAccount%20Name:%0AAccount%20Number:%0AAttach%20latest%203%20months%20bank%20statement.",
 };
 
 
